c8821abd93ad5b55795a9bbcb8c969af6deb3735
roms13 <romain.kelifa@gmail.com>
2019-08-29 14:18:30 +0700

    :arrow_up: Update dependencies

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const generate = async () => {
     const { gitmojis } = await fetch()
     await rules(gitmojis)
     await template(gitmojis)
-    console.log(`succesfully auto-generated semantic-release rules and github release template from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json`)
+    console.log('succesfully auto-generated semantic-release rules and github release template from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
   } catch (e) {
-    console.log(`error trying to auto-generate semantic-release rules and github release template from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json`)
+    console.log('error trying to auto-generate semantic-release rules and github release template from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
   }
 }
 generate()
